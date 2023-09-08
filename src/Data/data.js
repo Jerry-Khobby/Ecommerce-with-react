360c7d084a0b788fca4d261d185caf0ec4563779
Jerry-Khobby <jerrymardeburg@gmail.com>
2023-09-08 11:32:34 -1200

    After editing the card section

--- src/Data/data.js
+++ src/Data/data.js
@@ -42,34 +42,6 @@ const products =[
         price: 39.99,
         description: "When it comes to choosing the perfect bag for a busy day, space is key. Throw on this adidas backpack, and carry all your gear with ease. A roomy main compartment and front and side pockets let you keep your essentials organised and accessible. Plus, it's decked out in Trefoil logo style, so you can show off your adidas love on all your daily adventures.This product is made with Primegreen, a series of high-performance recycled materials.,"
       },
-      {
-        id: 7,
-        name: 'Product 7',
-        image: 'https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcRIA6sJOg2Bp0dbapbOEKX5YBh9lFmZ3dahnD2Gg7rDEG_Xtnn_THqs6srrNAIJba62YUkg5GoXxg8zHCzq_w26sD0HYLQvkUEjR0S7hArDCGU7E0fiVGBLH_KjbR0zJchJqE5jP-9QlXU&usqp=CAc',
-        price: 39.99,
-        description: "When it comes to choosing the perfect bag for a busy day, space is key. Throw on this adidas backpack, and carry all your gear with ease. A roomy main compartment and front and side pockets let you keep your essentials organised and accessible. Plus, it's decked out in Trefoil logo style, so you can show off your adidas love on all your daily adventures.This product is made with Primegreen, a series of high-performance recycled materials.,"
-      },
-      {
-        id: 8,
-        name: 'Product 8',
-        image: 'https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcRIA6sJOg2Bp0dbapbOEKX5YBh9lFmZ3dahnD2Gg7rDEG_Xtnn_THqs6srrNAIJba62YUkg5GoXxg8zHCzq_w26sD0HYLQvkUEjR0S7hArDCGU7E0fiVGBLH_KjbR0zJchJqE5jP-9QlXU&usqp=CAc',
-        price: 39.99,
-        description: "When it comes to choosing the perfect bag for a busy day, space is key. Throw on this adidas backpack, and carry all your gear with ease. A roomy main compartment and front and side pockets let you keep your essentials organised and accessible. Plus, it's decked out in Trefoil logo style, so you can show off your adidas love on all your daily adventures.This product is made with Primegreen, a series of high-performance recycled materials.,"
-      },
-      {
-        id: 9,
-        name: 'Product 9',
-        image: 'https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcRIA6sJOg2Bp0dbapbOEKX5YBh9lFmZ3dahnD2Gg7rDEG_Xtnn_THqs6srrNAIJba62YUkg5GoXxg8zHCzq_w26sD0HYLQvkUEjR0S7hArDCGU7E0fiVGBLH_KjbR0zJchJqE5jP-9QlXU&usqp=CAc',
-        price: 39.99,
-        description: "When it comes to choosing the perfect bag for a busy day, space is key. Throw on this adidas backpack, and carry all your gear with ease. A roomy main compartment and front and side pockets let you keep your essentials organised and accessible. Plus, it's decked out in Trefoil logo style, so you can show off your adidas love on all your daily adventures.This product is made with Primegreen, a series of high-performance recycled materials.,"
-      },
-      {
-        id: 10,
-        name: 'Product 10',
-        image: 'https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcRIA6sJOg2Bp0dbapbOEKX5YBh9lFmZ3dahnD2Gg7rDEG_Xtnn_THqs6srrNAIJba62YUkg5GoXxg8zHCzq_w26sD0HYLQvkUEjR0S7hArDCGU7E0fiVGBLH_KjbR0zJchJqE5jP-9QlXU&usqp=CAc',
-        price: 39.99,
-        description: "When it comes to choosing the perfect bag for a busy day, space is key. Throw on this adidas backpack, and carry all your gear with ease. A roomy main compartment and front and side pockets let you keep your essentials organised and accessible. Plus, it's decked out in Trefoil logo style, so you can show off your adidas love on all your daily adventures.This product is made with Primegreen, a series of high-performance recycled materials.,"
-      },
 ]
 
 
--- src/components/frontPageBody/productSession.jsx
+++ src/components/frontPageBody/productSession.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import products from '../../Data/data';
 import './productSession.css';
 import { Link } from 'react-router-dom';
+import Card from 'react-bootstrap/Card';
+
 
 
 
@@ -12,18 +14,23 @@ import { Link } from 'react-router-dom';
 
 const ProductSession = () => {
     return ( 
-        <div className="product-list">
+        <div className="product-list-heading">
+          <div className='heading-featured'>
+            <h3>Featured Products</h3>
+          </div>
+          <div className='product-list'>
   {products.map((product) => (
     <Link to={`/cart/${product.id}`} style={{textDecoration:'none',color:'inherit'}}>
-    <div className="product-card" key={product.id}>
-      <img src={product.image} alt={product.name} className="product-image" />
-      <div className="product-details">
-        <h2 className="product-name">{product.name}</h2>
-        <p className="product-price">${product.price}</p>
-      </div>
-    </div>
+    <Card  className="product-card" key={product.id}>
+      <Card.Img src={product.image} alt={product.name} className="product-image" />
+      <Card.Body className="product-details">
+        <Card.Title className="product-name">{product.name}</Card.Title>
+        <Card.Text className="product-price">${product.price}</Card.Text>
+      </Card.Body>
+    </Card>
     </Link>
   ))}
+</div>
 </div>
 
      );
